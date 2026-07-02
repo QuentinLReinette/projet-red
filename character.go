7db68331734eeebc9fe7775d8b8ac908b5c9c47a
QuentinLReinette <185085787+QuentinLReinette@users.noreply.github.com>
2026-07-02 09:56:53 +0200

    add money

--- character.go
+++ character.go
@@ -6,19 +6,19 @@ import (
 )
 
 type character struct {
-	name               string
-	maxHP, currHP, lvl int
+	name                     string
+	maxHP, currHP, lvl, gold int
 	class
 	*inventory
 	skills []skill
 }
 
 func initChar(name string, class class, maxHP int, newInventory *inventory) *character {
-	return &character{name: name, maxHP: maxHP, currHP: maxHP * 4 / 10, lvl: 1, inventory: newInventory, class: class}
+	return &character{name: name, maxHP: maxHP, currHP: maxHP * 4 / 10, lvl: 1, gold: 100, inventory: newInventory, class: class}
 }
 
 func (c *character) displayInfo() {
-	fmt.Printf(" Name: %s\n HP: %d/%d\n Class: %s\n", c.name, c.currHP, c.maxHP, c)
+	fmt.Printf(" Name: %s\n HP: %d/%d\n Class: %s\n Gold: %d\n", c.name, c.currHP, c.maxHP, c, c.gold)
 }
 
 func (c *character) checkAlive() {
--- class.go
+++ class.go
@@ -19,13 +19,13 @@ func (c class) String() string {
 }
 
 func newHuman(name string) *character {
-	return &character{name, 100, 50, 1, human, &inventory{}, []skill{}}
+	return &character{name, 100, 50, 1, 100, human, &inventory{}, []skill{}}
 }
 
 func newElf(name string) *character {
-	return &character{name, 80, 40, 1, elf, &inventory{}, []skill{}}
+	return &character{name, 80, 40, 1, 100, elf, &inventory{}, []skill{}}
 }
 
 func newDwarf(name string) *character {
-	return &character{name, 120, 60, 1, dwarf, &inventory{}, []skill{}}
+	return &character{name, 120, 60, 1, 100, dwarf, &inventory{}, []skill{}}
 }
